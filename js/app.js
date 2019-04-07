28048bd3eafefb071e0d6a1d099f489670000ddc
Artur Trifonov <artur@uniteddesign.ru>
2019-04-07 21:51:20 +0300

    start work on alerts

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //=require app/util/add-element.js
 
 // Site apps
+//=require app/error/show.js
 //=require app/forms/filled.js
 //=require app/forms/validation.js
 //=require app/controls/switches.js
