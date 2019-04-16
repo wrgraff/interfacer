cebd0d2ca4e1c9d8b29224a25a1f423a1866c44c
Artur Trifonov <artur@uniteddesign.ru>
2019-04-16 23:57:51 +0200

    added buttons listener and content loader

--- js/app/modals/call.js
+++ js/app/modals/call.js
@@ -1,57 +1,104 @@
-var modals = function() {
-    // Get elements
-    var modalButtons = document.querySelectorAll('[data-modal]');
-    console.log(modalButtons);
+var modalCloseButton = '<button type="button" class="ico-button"><span class="ico"><img src="/img/ico/baseline-close-24px.svg" alt="Иконка закрытия" /></span></button>';
 
-    createModalWindow();
+var closeWindow = function(modalWindow, modalMask) {
+    modalWindow.classList.add('modal-window_closed');
+    modalMask.classList.add('modal-mask_closed');
 
-    var element, source, container;
-    modalButtons.forEach((button) => {
-        button.addEventListener('click', function() {
-            element = button.tagName,
-            source = button.dataset.modalSource || 'inline',
-            container = document.querySelector('[data-modal-' + (button.dataset.modalContainer || 'window') + ']');
-
-            console.log('element: ' + element + ', source: ' + source + ', container: ' + container);
-
-            toggleModal(container);
-            loadContent(button, element, source, container);
-
-            var innerCloseButtons = container.querySelector('[data-modal-container]').querySelectorAll('[data-modal-close]');
-            innerCloseButtons.forEach((innerCloseButton) => {
-                innerCloseButton.addEventListener('click', () => {
-                    toggleModal(container);
-                }, false);
-            });
-
-        });
-    });
+    setTimeout(function() {
+        modalWindow.remove();
+        modalMask.remove();
+    }, 500);
 };
-var createModalWindow = function() {
+var createModalWindow = function(windowContent) {
     var modalWindow = addElement('div', 'modal-window'),
-        modalMask = addElement('div', 'modal-mask');
+        modalMask = addElement('div', 'modal-mask'),
+        modalClose = addElement('div', 'modal-window__close');
 
-    modalWindow.append(addElement('div', 'modal-window__container'));
-
-    var modalClose = addElement('div', 'modal-modal-window__close');
-    modalClose.innerHTML = '<button type="button" class="ico-button"><span class="ico"><img src="/img/ico/baseline-close-24px.svg" alt="Иконка закрытия" /></span></button>';
+    modalClose.innerHTML = modalCloseButton;
+    if (windowContent) {
+        modalWindow.innerHTML = windowContent;
+    } else {
+        showAlert('Content for modal window not found');
+        return false;
+    }
     modalWindow.append(modalClose);
 
     document.body.append(modalWindow);
     document.body.append(modalMask);
+
+    modalWindow.classList.add('modal-window_opened');
+    modalMask.classList.add('modal-mask_opened');
+
+    modalClose.addEventListener('click', function() {
+        closeWindow(modalWindow, modalMask);
+    });
+    modalMask.addEventListener('click', function() {
+        closeWindow(modalWindow, modalMask);
+    });
 };
 
-var callModal = function() {
+var modalButtons = document.querySelectorAll('[data-modal]');
+// console.log(modalButtons);
+modalButtons.forEach((button) => {
+    if (button.tagName === 'A') {
+        var contentAddress = button.getAttribute('href');
+    } else {
+        var contentAddress = button.dataset.modal;
+    };
+    contentAddress = contentAddress.indexOf('#') == 0 ? contentAddress.substring(1) : contentAddress;
+    // console.log('contentAddress: ' + contentAddress);
+    var windowContent = document.querySelector('#' + contentAddress) || false;
+    // console.log('windowContent: ' + windowContent);
+    var windowContentHTML = windowContent.innerHTML;
+    // console.log('windowContentHTML: ' + windowContentHTML);
 
-};
-var toggleActive = function(element) {
-    element.classList.toggle(element.classList.item(0) + '_active');
-};
-var toggleModal = function(container) {
-    console.log('toggle modal ' + container);
-    toggleActive(container);
-    toggleActive(modalMask);
-};
+    button.addEventListener('click', function() {
+        createModalWindow(windowContentHTML);
+    });
+});
+
+// var modals = function() {
+//     // Get elements
+//     var modalButtons = document.querySelectorAll('[data-modal]');
+//     console.log(modalButtons);
+//
+//     createModalWindow();
+//
+//     var element, source, container;
+//     modalButtons.forEach((button) => {
+//         button.addEventListener('click', function() {
+//             element = button.tagName,
+//             source = button.dataset.modalSource || 'inline',
+//             container = document.querySelector('[data-modal-' + (button.dataset.modalContainer || 'window') + ']');
+//
+//             console.log('element: ' + element + ', source: ' + source + ', container: ' + container);
+//
+//             toggleModal(container);
+//             loadContent(button, element, source, container);
+//
+//             var innerCloseButtons = container.querySelector('[data-modal-container]').querySelectorAll('[data-modal-close]');
+//             innerCloseButtons.forEach((innerCloseButton) => {
+//                 innerCloseButton.addEventListener('click', () => {
+//                     toggleModal(container);
+//                 }, false);
+//             });
+//
+//         });
+//     });
+// };
+
+//
+// var callModal = function() {
+//
+// };
+// var toggleActive = function(element) {
+//     element.classList.toggle(element.classList.item(0) + '_active');
+// };
+// var toggleModal = function(container) {
+//     console.log('toggle modal ' + container);
+//     toggleActive(container);
+//     toggleActive(modalMask);
+// };
 
     // modalCloseButtons = document.querySelectorAll('[data-modal-close]'),
     // modalAside = document.querySelector('[data-modal-aside]'),
@@ -112,4 +159,5 @@ var toggleModal = function(container) {
 //     toggleModal(container);
 // }, false);
 
-modals();
+// modals();
+//
