cb23c196ea4e078c6a0b1a4b73d2b5f7a26a0db2
wrgraff <wrgraff@gmail.com>
2020-06-13 05:00:47 +0300

    reorginized system

---
+++ blocks/button/README.md
@@ -0,0 +1,12 @@
+# Button
+Optimized for buttons and links
+
+## Button tag
+```
+<button type="button" class="button">Button</button>
+```
+
+## Link tag
+```
+<a href="" class="button">Button</a>
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,23 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify-es').default,
-    concat = require('gulp-concat'),
-    include = require("gulp-include"),
-    nunjucks = require('gulp-nunjucks-render'),
-	prettier = require('gulp-prettier'),
-	browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+	  browserSync = require('browser-sync').create();
 
-gulp.task('scss', function () {
-    return gulp.src('src/scss/app.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
-});
-gulp.task('html', function () {
-    return gulp.src('**/*.html').pipe(browserSync.stream());
-});
-gulp.task('js', function() {
-    return gulp.src('src/js/app.js')
-        .pipe(include())
-        // .pipe(uglify())
-        .on('error', console.log)
-        .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('docs', () =>
-    gulp.src('src/njk/pages/**/*.njk')
-        .pipe(nunjucks({
-            path: ['src/njk/layouts']
+const scss = () => {
+    return gulp.src('blocks/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(function (file) {
+            return file.base;
         }))
-        .pipe(prettier({ proseWrap: 'never', printWidth: 800, tabWidth: 4, useTabs: true }))
-        .pipe(gulp.dest('docs'))
-);
+        .pipe(browserSync.stream());
+};
+exports.scss = scss;
 
-gulp.task('default', function () {
+const serve = () => {
 	browserSync.init({
-        server: "./dist"
+        server: "./"
     });
-    gulp.watch('src/scss/**/*', gulp.series('scss')).on('change', browserSync.reload);
-    gulp.watch('**/*.html', gulp.series('html')).on('change', browserSync.reload);
-    gulp.watch('src/js/**/*.js', gulp.series('js')).on('change', browserSync.reload);
-    gulp.watch('src/njk/**/*.njk', gulp.series('docs'));
-});
+
+    gulp.watch('blocks/**/*.scss', gulp.series(scss));
+};
+exports.serve = serve;
+exports.default = serve;
